08f7622e072b63c92c0d95720951a65230a479b9
Sam Fortin <sam.j.fortin@gmail.com>
2017-09-04 12:33:49 -0500

    Past work page update

--- server/public/scripts/controllers/past-work.controller.js
+++ server/public/scripts/controllers/past-work.controller.js
@@ -2,4 +2,89 @@ PortfolioApp.controller('PastWorkController', function () {
     console.log('PastWorkController loaded.')
     var vm = this;
 
+    vm.desktop = [{
+        name: 'Comic Con',
+        image: 'comic-con-2017.png'
+    },
+    {
+        name: 'boston-scientific-smm.png',
+        image: 'boston-scientific-smm.png'
+    },
+    {
+        name: 'nbc-mcr.png',
+        image: 'nbc-mcr.png'
+    },
+    {
+        name: 'revista.png',
+        image: 'revista.png'
+    },
+    {
+        name: 'clinton-foundation.png',
+        image: 'clinton-foundation.png'
+    },
+    {
+        name: 'nbc-premier-league.png',
+        image: 'nbc-premier-league.png'
+    },
+    {
+        name: 'comic-con-2017.png',
+        image: 'comic-con-2017.png'
+    },
+    {
+        name: 'Disney Shanghai',
+        image: 'disney-shanghai.png'
+    },
+    {
+        name: 'digital-river-livestream.png',
+        image: 'digital-river-livestream.png'
+    },
+    {
+        name: 'target-partner-summit.png',
+        image: 'target-partner-summit.png'
+    },
+    {
+        name: 'disney-gcat.png',
+        image: 'disney-gcat.png'
+    },
+    {
+        name: 'wells-fargo-livestream.png',
+        image: 'wells-fargo-livestream.png'
+    },
+    {
+        name: 'nbc-mcm.png',
+        image: 'nbc-mcm.png'
+    },
+    {
+        name: 'pentair-glm.png',
+        image: 'pentair-glm.png'
+    },
+    {
+        name: 'Patterson',
+        image: 'patterson-gravity.png'
+    },
+    {
+        name: 'NBC Olympics',
+        image: 'nbc-olympic-summit.png'
+    }
+    ]
+
+    vm.mobile = [
+        {
+            name: 'beautycounter',
+            image: 'beautycounter-mobile.png'
+        },
+        {
+            name: 'neda-mobile.png',
+            image: 'neda-mobile.png'
+        },
+        {
+            name: 'nylc-mobile.png',
+            image: 'nylc-mobile.png'
+        },
+        {
+            name: 'pentair-glm-mobile.png',
+            image: 'pentair-glm-mobile.png'
+        }
+    ]
+
 });
