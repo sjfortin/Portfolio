49d473f8a0e1af384808dc567b91b02f21ab0602
Sam Fortin <sam.j.fortin@gmail.com>
2017-09-04 22:08:43 -0500

    Repo search and order by update

--- server/public/scripts/controllers/repo.controller.js
+++ server/public/scripts/controllers/repo.controller.js
@@ -2,11 +2,31 @@ PortfolioApp.controller('RepoController', ['GithubService', function (GithubServ
     console.log('RepoController loaded.')
     var vm = this;
     vm.repos = GithubService.repos;
-    
+    vm.orderBy = '-created_at';
+
+    vm.orderByRepoName = function () {
+
+        if (vm.orderBy === 'name') {
+            vm.orderBy = '-name';
+        } else {
+            vm.orderBy = 'name';
+        }
+        console.log('vm.orderBy', vm.orderBy);
+    }
+    vm.orderByCreatedDate = function () {
+
+        if (vm.orderBy === '-created_at') {
+            vm.orderBy = 'created_at';
+        } else {
+            vm.orderBy = '-created_at';
+        }
+        console.log('vm.orderBy', vm.orderBy);
+    }
+
     // Search repo feature
     // vm.searchRepo = {};
     // vm.searchRepos = function () {
     //     vm.searchRepo = vm.search;
     // }
-    
+
 }]);
